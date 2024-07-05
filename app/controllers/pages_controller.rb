d3feba0cd0381ada55c406b0d8e4467039413830
thiagohrcosta <thiagohrcosta86@gmail.com>
2024-07-05 20:42:39 -0300

    adding home layout

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,11 +1,14 @@
 class PagesController < ApplicationController
+  skip_before_action :authenticate_user!, only: :index
+
   def index
     @last_videos = VideoContent.all.map do |video|
       video_id = extract_youtube_id(video.url)
       {
         id: video_id,
         title: video.title,
-        content: video.content
+        content: video.content,
+        video_id: video.id
       }
     end
   end
